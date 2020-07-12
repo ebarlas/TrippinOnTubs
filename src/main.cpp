ccd85145c4f5865cd0b77b661ae2c552c8746f37
Elliot Barlas <elliotbarlas@gmail.com>
2020-07-11 21:35:22 -0700

    Move demo applications to subdirectory.

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <tuple>
-#include <unordered_set>
-#include "engine/Point.h"
-
-int main() {
-    using quad = std::pair<int, int>;
-    quad t{1, 2};
-    std::unordered_set<std::pair<int, int>> x;
-}
