93d653006fec416af9a9559da893b347dbd0044a
Elliot Barlas <elliotbarlas@gmail.com>
2020-08-08 10:33:25 -0700

    Add comments to Object header.

--- src/engine/Object.h
+++ src/engine/Object.h
@@ -18,23 +18,63 @@ namespace trippin {
         friend class SnapQueue;
 
     protected:
+        // A unique identifier for this object.
         int id{};
+
+        // A flag indicating whether this object is a platform.
+        // Platforms cannot collide with each other and platforms have the highest priority during object snapping.
         bool platform{};
+
+        // The mass of this objects. This is important for calculating collision reactions.
         double mass{};
+
+        // The top left corner position of the object in abstract units.
         Point<double> position{};
-        Point<double> center{};
-        Point<double> velocity{};
-        Point<double> acceleration{};
-        Point<double> terminalVelocity{};
-        Point<double> friction{};
-        Point<double> gravity{};
-        double fallGravity{};
+
+        // The size of the object in abstract units.
         Point<int> size{};
 
+        // The velocity of the object in units per engine tick.
+        Point<double> velocity{};
+
+        // The acceleration of the object in units per engine tick per engine tick.
+        Point<double> acceleration{};
+
+        // The terminal velocity of the object in units per engine tick.
+        // The object cannot exceed the maximum in either dimension.
+        Point<double> terminalVelocity{};
+
+        // The deceleration due to friction in units per engine tick per engine tick.
+        // This quantity is applied when an object is in contact with an absorbent platform.
+        Point<double> friction{};
+
+        // The acceleration due to gravity in units per engine tick per engine tick.
+        // This quantity is applied continuously, except when an object is in contact with a platform.
+        Point<double> gravity{};
+
+        // The acceleration due to gravity applied to falling objects in units per engine tick per engine tick.
+        // This quantity is applied continuously, except when an object is in contact with a platform.
+        // This is used instead of (not in addition to) the normal gravity.
+        // A value of 0 indicated that it is unset.
+        double fallGravity{};
+
+        // The rounded top left corner position of the object.
+        // This is a meta property based on position.
         Point<int> roundedPosition{};
+
+        // The center position of the object.
+        // This is a meta property based on position and size.
+        Point<double> center{};
+
+        // The rounded center of the object.
+        // This is a meta property based on position and size.
         Point<int> roundedCenter{};
+
+        // The rounded bounding box of the object.
+        // This is a meta property based on position and size.
         Rect<int> roundedBox{};
 
+        // Optional platform collision type, which overrides engine default.
         Optional<PlatformCollisionType> platformCollisionType{};
 
         // Flag used by snap priority queue.
