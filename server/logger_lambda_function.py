2cec6473d87a2afa955ab21c1c3bf82452c6d50f
Elliot Barlas <elliotbarlas@gmail.com>
2022-06-28 16:24:48 -0700

    Update log server Python Lambda Function.

--- server/logger_lambda_function.py
+++ server/logger_lambda_function.py
@@ -18,23 +18,20 @@ def validate(event):
 
     params = urllib.parse.parse_qs(event['querystring'])  # querystring request property is a raw string
     record = extract_body(event)
-    if 'id' not in record or 'time' not in record or 'index' not in record or 'message' not in record:
+    if 'id' not in record or 'index' not in record or 'message' not in record:
         return False
     id = record['id']
-    start_time = record['time']
     index = record['index']
     message = record['message']
 
-    if type(id) is not int or type(start_time) is not int or type(index) is not int or type(message) is not str:
-        return False
-    if id < 0 or id > 2 ** 64:
+    if type(id) is not str or type(index) is not int or type(message) is not str:
         return False
 
     if 'h' not in params:
         return False
 
     hash = params['h'][0]
-    hashgen = hex(id)[2:] + hex(start_time)[2:] + hex(index)[2:]
+    hashgen = str(sum(bytearray(id + str(index), 'ascii')))
     if hash != hashgen:
         return False
 
@@ -43,20 +40,18 @@ def validate(event):
 
 def add_log_record(record):
     id = record['id']
-    start_time = record['time']
     index = record['index']
     message = record['message']
     epoch = int(time.time())
     date = datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d %H:%M:%S')
     item = {
         'pk': {'N': '1'},
-        'sk': {'S': f'{start_time}-{id}-{index}'},
-        'time': {'N': str(epoch)},
+        'sk': {'S': f'{id}/{index}'},
         'date': {'S': date},
         'message': {'S': message}
     }
     client.put_item(TableName='trippin-logs', Item=item)
-    print(f'added log entry, appid={start_time}-{id}, index={index}, message={message}')
+    print(f'added log entry, appid={id}, index={index}, message={message}')
 
 
 def lambda_handler(event, context):
