3cfb44c7b99d402b6818e089c017ac448d66e7e0
Elliot Barlas <elliotbarlas@gmail.com>
2022-06-29 19:30:35 -0700

    Update staging area unit tests.

--- test/net/staging_area_tests.cpp
+++ test/net/staging_area_tests.cpp
@@ -4,21 +4,21 @@
 TEST_CASE("Combine distinct", "[staging]")
 {
     trippin::StagingArea sa;
-    sa.addScore({12345, 123, "ABCDE"});
-    sa.setTodayScores({{23456, 456, "FGHIJ"}});
+    sa.addScore({"abc123", 1, 100, "ABC", {}});
+    sa.setTodayScores({{"def456", 1, 200, "DEF", {}}});
     auto scores = sa.getTodayScores(10);
     REQUIRE(scores.size() == 2);
-    REQUIRE(scores[0].name == "FGHIJ");
-    REQUIRE(scores[1].name == "ABCDE");
+    REQUIRE(scores[0].name == "DEF");
+    REQUIRE(scores[1].name == "ABC");
 }
 
 TEST_CASE("Combine collision", "[staging]")
 {
     trippin::StagingArea sa;
-    trippin::Score ds{12345, 123, "ABCDE"};
+    trippin::Score ds{"abc123", 1, 100, "ABC", {}};
     sa.addScore(ds);
     sa.setTodayScores({ds});
     auto scores = sa.getTodayScores(10);
     REQUIRE(scores.size() == 1);
-    REQUIRE(scores[0].name == "ABCDE");
+    REQUIRE(scores[0].name == "ABC");
 }
