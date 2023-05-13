980d407f66535403d052d630ba978384ad964736
Elliot Barlas <elliotbarlas@gmail.com>
2023-05-12 21:58:02 -0700

    Change Android project directory name.

--- android/build.sh
+++ android/build.sh
@@ -14,26 +14,28 @@ curl -L https://github.com/nlohmann/json/archive/v3.11.2.tar.gz | tar -xzv
 
 mv SDL2-2.26.5/android-project/ .
 
-mv SDL2* android-project/app/jni/
-mv json* android-project/app/jni/
+mv android-project trippin
 
-mkdir android-project/app/src/main/assets
-cp -R ../../src/* android-project/app/jni/src/
-cp -R ../../sprites android-project/app/src/main/assets/
-cp -R ../../config android-project/app/src/main/assets/
-cp -R ../../maps android-project/app/src/main/assets/
-cp -R ../../sounds android-project/app/src/main/assets/
-cp -R ../../autoplay android-project/app/src/main/assets/
-cp ../Android.mk android-project/app/jni/src/
-cp ../Application.mk android-project/app/jni/
-cp ../nlohmann_Android.mk android-project/app/jni/json-3.11.2/Android.mk
-sed -i.bak 's/\(.*<activity.*\)/\1 android:screenOrientation="landscape"/g' android-project/app/src/main/AndroidManifest.xml
-sed -i.bak 's/\(.*<application .*\)/\<uses-permission android:name="android.permission.INTERNET" \/\> \1/g' android-project/app/src/main/AndroidManifest.xml
-sed -i.bak 's/super.onCreate.*/&\n        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {\n            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;\n        }/g' android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
+mv SDL2* trippin/app/jni/
+mv json* trippin/app/jni/
+
+mkdir trippin/app/src/main/assets
+cp -R ../../src/* trippin/app/jni/src/
+cp -R ../../sprites trippin/app/src/main/assets/
+cp -R ../../config trippin/app/src/main/assets/
+cp -R ../../maps trippin/app/src/main/assets/
+cp -R ../../sounds trippin/app/src/main/assets/
+cp -R ../../autoplay trippin/app/src/main/assets/
+cp ../Android.mk trippin/app/jni/src/
+cp ../Application.mk trippin/app/jni/
+cp ../nlohmann_Android.mk trippin/app/jni/json-3.11.2/Android.mk
+sed -i.bak 's/\(.*<activity.*\)/\1 android:screenOrientation="landscape"/g' trippin/app/src/main/AndroidManifest.xml
+sed -i.bak 's/\(.*<application .*\)/\<uses-permission android:name="android.permission.INTERNET" \/\> \1/g' trippin/app/src/main/AndroidManifest.xml
+sed -i.bak 's/super.onCreate.*/&\n        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {\n            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;\n        }/g' trippin/app/src/main/java/org/libsdl/app/SDLActivity.java
 
 # SDLActivity invokes nativeRunMain JNI function
 # nativeRunMain, defined in SDL_android.c, looks for SDL_main function
 # Troubleshoot using objdump, for ex objdump -T app/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/libmain.so | grep SDL_main
-sed -i.bak 's/int main/int SDL_main/g' android-project/app/jni/src/trippin/trippin.cpp
+sed -i.bak 's/int main/int SDL_main/g' trippin/app/jni/src/trippin/trippin.cpp
 
 popd
--- android/update.sh
+++ android/update.sh
@@ -5,20 +5,20 @@ fi
 
 pushd "${1}"
 
-rm -rf android-project/app/src/main/assets/*
-rm -rf android-project/app/jni/src/*
+rm -rf trippin/app/src/main/assets/*
+rm -rf trippin/app/jni/src/*
 
-cp -R ../Android.mk android-project/app/jni/src/
-cp -R ../../src/* android-project/app/jni/src/
-cp -R ../../config android-project/app/src/main/assets/
-cp -R ../../maps android-project/app/src/main/assets/
-cp -R ../../sprites android-project/app/src/main/assets/
-cp -R ../../sounds android-project/app/src/main/assets/
-cp -R ../../autoplay android-project/app/src/main/assets/
+cp -R ../Android.mk trippin/app/jni/src/
+cp -R ../../src/* trippin/app/jni/src/
+cp -R ../../config trippin/app/src/main/assets/
+cp -R ../../maps trippin/app/src/main/assets/
+cp -R ../../sprites trippin/app/src/main/assets/
+cp -R ../../sounds trippin/app/src/main/assets/
+cp -R ../../autoplay trippin/app/src/main/assets/
 
 # SDLActivity invokes nativeRunMain JNI function
 # nativeRunMain, defined in SDL_android.c, looks for SDL_main function
 # Troubleshoot using objdump, for ex objdump -T app/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/libmain.so | grep SDL_main
-sed -i.bak 's/int main/int SDL_main/g' android-project/app/jni/src/trippin/trippin.cpp
+sed -i.bak 's/int main/int SDL_main/g' trippin/app/jni/src/trippin/trippin.cpp
 
 popd
