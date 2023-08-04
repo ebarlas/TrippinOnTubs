ee3c2607cbc3a985e7c72ec8fec199967ea9e6f4
Elliot Barlas <elliotbarlas@gmail.com>
2023-08-04 15:08:26 -0700

    Add first draft of sherlock log streaming and log analysis tool.

---
+++ tools/sherlock.py
@@ -0,0 +1,79 @@
+import datetime
+import re
+import time
+import zoneinfo
+
+import boto3
+
+client = boto3.client('dynamodb', region_name='us-west-2')
+
+table = 'trippin-logs'
+
+epoch = int(time.time())
+date = datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d %H:%M:%S')
+
+
+def query(date, last_key=None):
+    params = {
+        'TableName': table,
+        'IndexName': 'date-index',
+        'Limit': 50,
+        'KeyConditionExpression': 'pk = :pk and #date > :date',
+        'ExpressionAttributeNames': {
+            '#date': 'date'
+        },
+        'ExpressionAttributeValues': {
+            ':pk': {
+                'N': '1'
+            },
+            ':date': {
+                'S': date
+            }
+        }
+    }
+    if last_key:
+        params['ExclusiveStartKey'] = last_key
+    return client.query(**params)
+
+
+def convert(date):
+    pattern = r'(\d+)-(\d+)-(\d+) (\d+):(\d+):(\d+)'
+    tz = zoneinfo.ZoneInfo('America/Los_Angeles')
+    utc = datetime.timezone.utc
+    m = re.match(pattern, date)
+    yr, mon, day, hr, min, sec = [int(g) for g in m.groups()]
+    d = datetime.datetime(year=yr, month=mon, day=day, hour=hr, minute=min, second=sec, tzinfo=utc).astimezone(tz=tz)
+    return d.strftime('%Y-%m-%d %H:%M:%S')
+
+
+def print_item(item):
+    id, index = item['sk']['S'].split('/')
+    date = convert(item['date']['S'])
+    msg = item['message']['S']
+    print(f'[{date}] id={id}, index={index}, {msg}')
+
+
+def scroll_date(date):
+    last_key = None
+    last_date = date
+    while True:
+        res = query(date, last_key)
+        items = res['Items']
+        if items:
+            last_date = items[-1]['date']['S']
+        for item in items:
+            print_item(item)
+        last_key = res.get('LastEvaluatedKey')
+        if not last_key:
+            break
+    return last_date
+
+
+def scroll(date):
+    while True:
+        date = scroll_date(date)
+        time.sleep(5)
+        print(f'sleeping...')
+
+
+scroll(date)
